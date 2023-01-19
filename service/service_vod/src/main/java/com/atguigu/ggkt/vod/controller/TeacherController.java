9af729d9d739f9dfb41bcf5ab503277502504ed8
chen peiyu <chenpeiyu2001@outlook.com>
2023-01-19 10:41:02 +0800

    第一个模块的构建

---
+++ service/service_vod/src/main/java/com/atguigu/ggkt/vod/controller/TeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.ggkt.vod.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author Chen Peiyu
+ * @since 2023-01-19
+ */
+@RestController
+@RequestMapping("/vod/teacher")
+public class TeacherController {
+
+}
+
